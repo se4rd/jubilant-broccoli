951ce2e69f35f82fc500d65a00ce20a41344f906
se4rd <udhakal970@gmail.com>
2025-01-16 14:41:33 +0545

    Perform handshake with the server for some protocol agreement

--- main.asm
+++ main.asm
@@ -5,6 +5,7 @@ _connect_to_X11:
 		
 	push rbp
 	mov rbp, rsp
+	sub rsp, 112
 
 	;; Opening a Socket
 
@@ -23,8 +24,6 @@ _connect_to_X11:
 
 	;; Storing struct sockaddr_un on the stack
 
-		sub rsp, 112
-
 		mov WORD [rsp], 1
 		lea rsi, [_data.sun_path]
 		lea rdi, [rsp + 2]
@@ -34,7 +33,7 @@ _connect_to_X11:
 
 	;; Connecting to the server
 
-		mov rax, 101
+		mov rax, 42
 		mov rdi, r12
 		lea rsi, [rsp]
 		mov rdx, 110
@@ -43,15 +42,114 @@ _connect_to_X11:
 		cmp rax, 0
 		jne	_exit_process 
 
-		mov rax, r12
-	
 	add rsp, 112
 	pop rbp
 	ret
 
+_send_handshake_to_X11:
+
+	push rbp
+	mov rbp, rsp
+	sub rsp, 32768
+
+	;; Create structure for handshake
+
+		mov BYTE [rsp + 0], 'l'
+		mov WORD [rsp + 2], 11
+
+	;; Send handshake to the server
+
+		mov rax, 1
+		mov rdi, r12
+		lea rsi, [rsp]
+		mov rdx, 12
+		syscall
+
+		cmp rax, 12
+		jnz _exit_process
+
+	;; Read the server response (8 bytes)
+
+		mov rax, 0
+		mov rdi, r12
+		lea rsi, [rsp]
+		mov rdx, 8
+		syscall
+
+		cmp rax, 8
+		jnz _exit_process
+
+	;; Check if server sent 'success' (first byte must be 1)
+
+		cmp BYTE [rsp], 1
+		jnz _exit_process
+
+	;; Read the rest of the server response
+
+		mov rax, 0
+		mov rdi, r12
+		mov rsi, rsp
+		mov rdx, 32768
+		syscall
+
+		cmp rax, 0
+		jle _exit_process
+
+	;; Set id_base 
+
+		mov edx, DWORD [rsp + 4]
+		mov DWORD [_data.id_base], edx
+
+	;; Set id_mask
+
+		mov edx, DWORD [rsp + 8]
+		mov DWORD [_data.id_mask], edx
+
+	;; Pointer that will skip over some data
+
+		mov rdi, rsp
+
+	;; Vendor length
+
+		mov cx, WORD [rsp + 16]
+		movzx rcx, cx
+
+	;; Number of formats | sizeof(each format) == 8
+
+		mov al, BYTE [rsp + 21]
+		movzx rax, al
+		imul rax, 8
+
+	;; Skip connection setup and vendor information
+
+		add rdi, 32
+		add rdi, rcx
+
+	;; Skip over padding
+
+		add rdi, 3
+		add rdi, -4
+
+	;; Skip over the format information
+
+		add rdi, rax
+
+	;; Store (Return) the window root id
+
+		mov eax, DWORD [rdi]
+
+	;; Set root_visual_id
+
+		mov edx, DWORD [rdi + 32]
+		mov DWORD [_data.root_visual_id], edx
+
+	add rsp, 32768
+	pop rbp
+	ret
+
 _start:
-	int3
 	call _connect_to_X11
+	call _send_handshake_to_X11
 
 _exit_process:
 	mov rdi, rax
@@ -64,3 +162,16 @@ _data:
 	db "/tmp/.X11-unix/X0", 0
 
 .path_len = $ - .sun_path
+
+.id:
+	dd 0
+
+.id_base:
+	dd 0
+	
+.id_mask:
+	dd 0
+
+.root_visual_id:
+	dd 0
+
